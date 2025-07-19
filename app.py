5b598d657e6103d4635510b31d93146364e4c6da
key1993 <amer_kiwan@hotmail.com>
2025-07-19 22:44:50 +0300

    Fix: bind to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from routes import routes
 from admin import admin
+import os
+
 
 app = Flask(__name__)
 app.register_blueprint(routes)
 app.register_blueprint(admin)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
+    app.run(host="0.0.0.0", port=port)
