122018956bca9ff7765e413f1f5c71a540981d61
key1993 <amer_kiwan@hotmail.com>
2025-07-20 13:24:31 +0300

    Delete something

--- routes.py
+++ routes.py
@@ -81,17 +81,6 @@ def cancel_reservation():
     send_whatsapp_message(f"❌ Reservation cancelled:\n🧑 User: {user_id}\n📅 Date: {date}\n🕒 Slot: {time_slot}")
     return jsonify({'message': 'Reservation cancelled successfully'}), 200
 
-@routes.route("/reservations/<id>", methods=["DELETE"])
-def delete_by_id(id):
-    try:
-        result = reservations_collection.delete_one({"_id": ObjectId(id)})
-    except:
-        return jsonify({"error": "Invalid ID format"}), 400
-    if result.deleted_count == 1:
-        return jsonify({"message": "Reservation deleted"}), 200
-    else:
-        return jsonify({"error": "Reservation not found"}), 404
-
 @routes.route('/update_status', methods=['POST'])
 def update_status():
     reservation_id = request.form['reservation_id']
