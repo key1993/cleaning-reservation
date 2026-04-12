bc9924ba98a7845a27c6fc172bb4b60a530c9db6
key1993 <amer_kiwan@hotmail.com>
2026-04-12 14:39:20 +0300

    Update db.py

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 
 
-MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://amerkiwan7723:oPQwW3nr3url61yM@cluster0.6jzip7b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://saraalthaher7723_db_user:5Errd8L7V2mawi9O@cluster0.7faafep.mongodb.net/?appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "cleaning_reservation")
 
 client = MongoClient(MONGO_URI)
